24de53430732029b6d81c4c264af9e88a9089059
youngmarisa <marisayoung2013@gmail.com>
2021-11-18 10:03:51 -0800

    added react components and switched react router dom to 5.3 v

---
+++ app/javascript/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react"
+
+import {
+  BrowserRouter, 
+  Switch,
+  Route
+}from 'react-router-dom'
+
+
+
+class App extends Component {
+  render () {
+    return (
+      <BrowserRouter>
+      <Switch>
+        <Route></Route>
+      </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App

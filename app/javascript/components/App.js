e18f9f08f207a5f6d264e6cf7eafe8468efa9e80
youngmarisa <marisayoung2013@gmail.com>
2021-11-18 16:31:23 -0800

    added routes for header nav link

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import AboutUs from './pages/AboutUs'
 
 
 import {
@@ -13,13 +14,21 @@ import {
 
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      animals: []
+    }
+  }
   render () {
     return (
       
+      
       <BrowserRouter>
-        <Header />
+        <Header {...this.props} />
         <Routes>
           <Route exact path = "/" element = { <Home /> } />
+          <Route path = "/aboutus" />
         </Routes>
         <Footer />
       </BrowserRouter>

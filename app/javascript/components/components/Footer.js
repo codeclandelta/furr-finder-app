c655ad55311466acda331caa0d46a772460de228
John Brady <johnbrady.dev@gmail.com>
2021-11-21 20:09:14 -0800

    Add reactstrap import code for col and row, add spacing for layout

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
+import { Row, Col } from 'reactstrap'
 
 class Footer extends Component {
-    render (){
-        return (
-            <>
-            <h1>Footer</h1>
-            </>
-
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Row>
+          <Col>
+            <div className='container-fluid'>
+              <h1>Footer</h1>
+            </div>
+          </Col>
+        </Row>
+      </React.Fragment>
+    )
+  }
 }
 
 export default Footer
--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
-import { Navbar, Nav, NavItem, NavbarText, Collapse } from 'reactstrap'
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavbarText,
+  Collapse,
+  Row,
+  Col,
+} from 'reactstrap'
 import { NavLink } from 'react-router-dom'
 
 class Header extends Component {
@@ -9,33 +17,37 @@ class Header extends Component {
 
     return (
       <React.Fragment>
-        <header className='main-wrapper color-yellow'>
-          <Navbar expand='md'>
-            <NavbarText className='px-4 logo'>Furr Finder</NavbarText>
-            <NavLink to='/' className='px-4'>
-              Home
-            </NavLink>
-            <Collapse navbar>
-              <Nav className='me-auto' navbar>
-                <NavItem>
-                  <NavLink to='/aboutus' className='px-4 nav-link'>
-                    About Us
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <a href={new_user_route} className='nav-link'>
-                    Sign Up
-                  </a>
-                </NavItem>
-                <NavItem>
-                  <a href={sign_in_route} className='nav-link'>
-                    Sign In
-                  </a>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </Navbar>
-        </header>
+        <Row>
+          <Col>
+            <header className='main-wrapper color-yellow'>
+              <Navbar expand='md'>
+                <NavbarText className='px-4 logo'>Furr Finder</NavbarText>
+                <NavLink to='/' className='px-4'>
+                  Home
+                </NavLink>
+                <Collapse navbar>
+                  <Nav className='me-auto' navbar>
+                    <NavItem>
+                      <NavLink to='/aboutus' className='px-4 nav-link'>
+                        About Us
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <a href={new_user_route} className='nav-link'>
+                        Sign Up
+                      </a>
+                    </NavItem>
+                    <NavItem>
+                      <a href={sign_in_route} className='nav-link'>
+                        Sign In
+                      </a>
+                    </NavItem>
+                  </Nav>
+                </Collapse>
+              </Navbar>
+            </header>
+          </Col>
+        </Row>
       </React.Fragment>
     )
   }

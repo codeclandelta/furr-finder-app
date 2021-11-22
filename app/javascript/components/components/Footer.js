d96d2abd2be020ed8a836f7ee65f6ca5e0718654
LEARN Academy <learnacademy@LEARNs-Air.lan>
2021-11-22 13:07:07 -0800

    footer corrections

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -4,23 +4,23 @@ import { NavLink } from 'react-router-dom'
 
 
 class Footer extends Component {
-    render (){
-        return (
-            <>
-            <footer>
+  render (){
+    return (
+      <>
+      <footer>
         <Navbar color='light' expand='md' light>
-          <NavbarText className='px-4'>&copy;2021Furr Finder</NavbarText>
-          <NavLink to='/' className='px-4'>
-            Home
-          </NavLink>
-            <Nav className='me-auto' navbar>
-                <NavLink to='components/AboutUs' className='px-4'>
-                  About Us
-                </NavLink>
-            </Nav>
-             </Navbar>
-        </footer>
-        </>
+        <NavbarText className='px-4'>&copy;2021Furr Finder</NavbarText>
+        <NavLink to='/' className='px-4'>
+          Home
+        </NavLink>
+        <Nav className='me-auto' navbar>
+        <NavLink to='components/AboutUs' className='px-4'>
+          About Us
+        </NavLink>
+        </Nav>
+        </Navbar>
+      </footer>
+      </>
     )
   }
 }
--- app/javascript/components/components/Header.test.js
+++ app/javascript/components/components/Header.test.js
@@ -8,7 +8,7 @@ Enzyme.configure({ adapter: new Adapter() })
 describe('When Header renders', () => {
   it('displays 2 NavLink', () => {
     const header = shallow(<Header />)
-    const headerNav = header.find('NavLink')cd
+    const headerNav = header.find('NavLink')
     expect(headerNav.length).toEqual(2)
   })
 })

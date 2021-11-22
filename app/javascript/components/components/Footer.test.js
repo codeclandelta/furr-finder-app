bccbd436be12bd96f7abf583b32b7726fe04dd2a
LEARN Academy <learnacademy@LEARNs-Air.lan>
2021-11-22 11:12:17 -0800

    footer test

--- app/javascript/components/components/Footer.test.js
+++ app/javascript/components/components/Footer.test.js
@@ -1,14 +1,14 @@
-// import React from 'react'
-// import Footer from './Footer'
-// import Enzyme, { shallow } from 'enzyme'
-// import Adapter from 'enzyme-adapter-react-16'
+import React from 'react'
+import Footer from './Footer'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-// Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() })
 
-// describe('When Header renders', () => {
-//   it('displays 2 NavLink', () => {
-//     const header = shallow(<Header />)
-//     const headerNav = header.find('NavLink')
-//     expect(headerNav.length).toEqual(2)
-//   })
-// })
+describe('When Footer renders', () => {
+  it('displays 2 NavLink', () => {
+    const footer = shallow(<Footer />)
+    const footerNav = footer.find('NavLink')
+    expect(footerNav.length).toEqual(2)
+  })
+})

814cc59932aec77aa516dcc3ef104dd565eaddf8
LEARN Academy <learnacademy@LEARNs-Air.lan>
2021-11-22 14:51:24 -0800

    footer fixes

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -9,15 +9,11 @@ class Footer extends Component {
       <>
       <footer>
         <Navbar color='light' expand='md' light>
-        <NavbarText className='px-4'>&copy;2021Furr Finder</NavbarText>
-        <NavLink to='/' className='px-4'>
-          Home
-        </NavLink>
-        <Nav className='me-auto' navbar>
-        <NavLink to='components/AboutUs' className='px-4'>
-          About Us
-        </NavLink>
-        </Nav>
+          <NavbarText className='px-4'>&copy;2021Furr Finder</NavbarText>
+          <NavLink to='/' className='px-4'>Home</NavLink> 
+          <Nav className='me-auto' navbar>
+            <NavLink to='components/AboutUs' className='px-4'>About Us</NavLink>
+          </Nav>
         </Navbar>
       </footer>
       </>

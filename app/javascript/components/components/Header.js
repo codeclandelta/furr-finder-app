34daa85a843dcdff690501484094b3829ffd0948
John Brady <johnbrady.dev@gmail.com>
2021-11-29 12:25:31 -0800

    add privacy and terms pages, cleaning up header and footer

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -9,6 +9,7 @@ import {
   Col,
 } from 'reactstrap'
 import { NavLink } from 'react-router-dom'
+import logo from '../assets/logo-furr-finder.png'
 
 class Header extends Component {
   render() {
@@ -19,7 +20,7 @@ class Header extends Component {
       <React.Fragment>
         <Row>
           <Col>
-            <header className='main-wrapper color-yellow'>
+            {/* <header className='main-wrapper color-yellow'>
               <Navbar expand='md'>
                 <NavbarText className='px-4 logo'>Furr Finder</NavbarText>
                 <NavLink to='/' className='px-4'>
@@ -45,7 +46,65 @@ class Header extends Component {
                   </Nav>
                 </Collapse>
               </Navbar>
+            </header> */}
+
+            {/* <!-- ***** Header Area Start ***** --> */}
+            <header
+              className='header-area header-sticky wow slideInDown main-wrapper color-teal'
+              data-wow-duration='0.75s'
+              data-wow-delay='0s'
+            >
+              <div className='container-fluid'>
+                <div className='row'>
+                  <div className='col-12'>
+                    <nav className='main-nav'>
+                      {/* <!-- ***** Logo Start ***** --> */}
+                      <a href='index.html' className='logo'>
+                        {/* <img src='assets/images/logo.png' alt='Chain App Dev' /> */}
+                        <img
+                          src={logo}
+                          className='logo'
+                          alt='Illustration of a dog and cat'
+                        />
+                      </a>
+                      {/* <!-- ***** Logo End ***** --> */}
+                      {/* <!-- ***** Menu Start ***** --> */}
+                      <ul className='nav'>
+                        <li className='scroll-to-section'>
+                          <a href='#top' className='active'>
+                            Home
+                          </a>
+                        </li>
+                        <li className='scroll-to-section'>
+                          <a href='#services'>Services</a>
+                        </li>
+                        <li className='scroll-to-section'>
+                          <a href='#about'>About</a>
+                        </li>
+                        <li className='scroll-to-section'>
+                          <a href='#pricing'>Pricing</a>
+                        </li>
+                        <li className='scroll-to-section'>
+                          <a href='#newsletter'>Newsletter</a>
+                        </li>
+                        <li>
+                          <div className='gradient-button'>
+                            <a id='modal_trigger' href='#modal'>
+                              <i className='fa fa-sign-in-alt'></i> Sign In Now
+                            </a>
+                          </div>
+                        </li>
+                      </ul>
+                      <a className='menu-trigger'>
+                        <span>Menu</span>
+                      </a>
+                      {/* <!-- ***** Menu End ***** --> */}
+                    </nav>
+                  </div>
+                </div>
+              </div>
             </header>
+            {/* <!-- ***** Header Area End ***** --> */}
           </Col>
         </Row>
       </React.Fragment>
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,32 +3,34 @@ import blackCat from '../assets/black-cat.png'
 import { Button, Form, FormGroup, Input, Label, Row, Col } from 'reactstrap'
 
 class Home extends Component {
-  
-    state = {
-      searchTerm: ''
-    }
+  state = {
+    searchTerm: '',
+  }
   handleChange = (value) => {
-    this.setState ({searchTerm: value})  
+    this.setState({ searchTerm: value })
   }
 
   handleClick = () => {
-    console.log('handleClick');
-    console.log(this.state);
-    fetch("https://api.petfinder.com/v2/animals")
-    .then(response => console.log(response))
+    console.log('handleClick')
+    console.log(this.state)
+    fetch('https://api.petfinder.com/v2/animals').then((response) =>
+      console.log(response)
+    )
   }
 
   render() {
-    
     return (
-
       <React.Fragment>
-        <h1>Home Page</h1>
-        <Input type="text"
-          onChange = {(e) => this.handleChange(e.target.value)}
-          ></Input>
-        <Button onClick = {() => this.handleClick()} 
-        >Submit</Button>
+        <Row>
+          <Col>
+            <h1>Home Page</h1>
+            <Input
+              type='text'
+              onChange={(e) => this.handleChange(e.target.value)}
+            ></Input>
+            <Button onClick={() => this.handleClick()}>Submit</Button>
+          </Col>
+        </Row>
       </React.Fragment>
     )
   }
